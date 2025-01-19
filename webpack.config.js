5c29175aaebb7b2123a511ab508b092b803e0dd3
tyler4400 <tyler4400@foxmail.com>
2025-01-19 22:57:27 +0800

    样式和图片处理

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,41 @@
 const path = require('node:path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "development",
   entry: {
-    index: './src/index.js',
+    bundle: './src/index.js',
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
+    filename: 'js/[name].[hash:8].js',
+    clean: true,
+  },
+  plugins: [
+      new htmlWebpackPlugin({
+        filename: "index.html",
+        template: path.resolve(__dirname, './src/index.html'),
+      })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test:/\.(png|svg|jpg|jpeg|gif)$/i,
+        // dependency: { not: ['url'] }, // 从 asset loader 中排除来自新 URL 处理的 asset
+        type: "asset/resource",
+        parser: { // 用 module.parser 在一个地方配置所有解析器的选项
+          dataUrlCondition: {
+            maxSize: 8 * 1024,
+          }
+        },
+        generator: {
+          filename: 'images/[name].[hash:6].[ext]',
+        }
+      }
+    ]
   }
 }
